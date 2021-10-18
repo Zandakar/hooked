26d517ab7ac4bfac597cf05cb1a493a16f69c749
Tom <tom.deruijter@hotmail.com>
2021-10-18 21:35:52 +0800

    added window swiching

--- hook.py
+++ hook.py
@@ -26,6 +26,15 @@ def onCapsD():
     keyboard.press('right')
     print('ctrl D')
 
+def onCapsC():
+    keyboard.send('windows + ctrl + left')
+    print('ctrl C')
+
+def onCapsV():
+    keyboard.send('windows + ctrl + right')
+    print('ctrl V')
+  
+
 
 keyboard.block_key('caps lock')
 keyboard.add_hotkey('caps lock + e', onPressE, suppress=True)
@@ -34,6 +43,8 @@ keyboard.add_hotkey('caps lock + w', onCapsW, suppress=True)
 keyboard.add_hotkey('caps lock + a', onCapsA, suppress=True)
 keyboard.add_hotkey('caps lock + s', onCapsS, suppress=True)
 keyboard.add_hotkey('caps lock + d', onCapsD, suppress=True)
+keyboard.add_hotkey('caps lock + c', onCapsC, suppress=True)
+keyboard.add_hotkey('caps lock + v', onCapsV, suppress=True)
 while True:
     import time
     time.sleep(1000000)
