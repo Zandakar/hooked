cd45ed8d71950f1c8159efb95a2e5cc682f6889f
Tom <tom.deruijter@hotmail.com>
2021-10-18 20:27:49 +0800

    made the hook work again

--- hook.py
+++ hook.py
@@ -1,18 +1,14 @@
 import keyboard
 
 # https://github.com/boppreh/keyboard
-# https://github.com/boppreh/keyboard/blob/master/keyboard/_winkeyboard.py
-
-def onRelease(e):
-    print("Released: " + e.name)
 
 def onPress(e):
-    print('captured caps lock')
+    print('onPress ' + e.name)
 
-keyboard.on_press_key('caps lock', onPress, suppress=True)
+# keyboard.on_press_key('caps lock', onPress, suppress=True)
 keyboard.on_press(onPress)
-keyboard.on_release(onRelease)
 
 while True:
     import time
     time.sleep(1000)
+w
