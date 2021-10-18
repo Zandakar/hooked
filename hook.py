8e7297678536e0f9ee9c37e19b493c63059ca6e2
Tom <tom.deruijter@hotmail.com>
2021-10-18 20:34:10 +0800

    capturing caps lock e combo

--- hook.py
+++ hook.py
@@ -6,9 +6,8 @@ def onPress(e):
     print('onPress ' + e.name)
 
 # keyboard.on_press_key('caps lock', onPress, suppress=True)
-keyboard.on_press(onPress)
-
+# keyboard.on_press(onPress)
+keyboard.add_hotkey('caps lock + e', print, args=['ctrl + e was pressed'])
 while True:
     import time
     time.sleep(1000)
-w
