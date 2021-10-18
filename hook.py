c924dfb1ed9adf3ec3fe9ef616d7a45575e75ba3
Tom <tom.deruijter@hotmail.com>
2021-10-18 20:41:17 +0800

    surpressing caps lock while still capturing event

--- hook.py
+++ hook.py
@@ -3,11 +3,17 @@ import keyboard
 # https://github.com/boppreh/keyboard
 
 def onPress(e):
-    print('onPress ' + e.name)
+    print('onPress ' + e)
+
+def onCapsLock(e):
+    print('caps')
 
 # keyboard.on_press_key('caps lock', onPress, suppress=True)
 # keyboard.on_press(onPress)
-keyboard.add_hotkey('caps lock + e', print, args=['ctrl + e was pressed'])
+keyboard.block_key('caps lock')
+keyboard.add_hotkey('caps lock + e', onPress, args=['ctrl + e was pressed'], suppress=True, trigger_on_release=True)
 while True:
     import time
     time.sleep(1000)
+
+eEEEeeeeEEEEeeWWWQWEEEEDD
