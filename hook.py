551069dcc9e121121feb04e98f9473d2d25b7b6c
Tom <tom.deruijter@hotmail.com>
2021-10-18 20:51:11 +0800

    working enter and backspace (maybe? hook lib is being weird)

--- hook.py
+++ hook.py
@@ -1,19 +1,34 @@
+from os import execle
 import keyboard
 
 # https://github.com/boppreh/keyboard
 
 def onPress(e):
+    keyboard.press('enter')
     print('onPress ' + e)
+
+def onCapsQ(e):
+    keyboard.press('backspace')
+    print('onPress ' + e)
+
+
+
 
 def onCapsLock(e):
     print('caps')
 
-# keyboard.on_press_key('caps lock', onPress, suppress=True)
+# keyboard.on_press_key('caps lock', onPress, suppress=True)        
 # keyboard.on_press(onPress)
 keyboard.block_key('caps lock')
 keyboard.add_hotkey('caps lock + e', onPress, args=['ctrl + e was pressed'], suppress=True, trigger_on_release=True)
+keyboard.add_hotkey('caps lock + q', onCapsQ, args=['ctrl + q was pressed'], suppress=True, trigger_on_release=True)
 while True:
     import time
     time.sleep(1000)
 
-eEEEeeeeEEEEeeWWWQWEEEEDD
+
+
+
+
+
+
