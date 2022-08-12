d7f90d2f8606a4368cd14d2902c0b6d1d30baeb8
Tom <tom.deruijter@hotmail.com>
2022-08-12 18:15:17 +0800

    refactor

--- hook.py
+++ hook.py
@@ -1,81 +1,124 @@
-# https://pynput.readthedocs.io/en/latest/keyboard.html
+#https://stackoverflow.com/questions/67505554/python-keyboard-module-add-hotkey-is-not-working-after-you-lock-windows-once-he
 
-# https://github.com/moses-palmer/pynput/issues/105
-
-
-#  todo
-# how to stop endless loop?
-#
-#
+import keyboard
+import mouse
+import time
+from threading import Timer
 
 
+import PySimpleGUI as sg
 
 
-from pynput.keyboard import Key, Controller, Listener
+layout = [[sg.Text("Tom's super cool keyboard hook")], [sg.Button("Close")]]
 
-keyboard = Controller()
-global listener
-global caps
-
-caps = False
-
-
-keyActionMap =  {
-    'w': Key.up,
-    'a': Key.left,
-    'd': Key.right,
-    's': Key.down,
-    'e': Key.enter,
-    'q': Key.backspace
-}
-
-
-def on_press(key):
-    global caps
-
-    try:
-        print(key)
-
-        if key == Key.caps_lock:    
-            caps = True
-
-        if caps == True:
-            print('if caps true')
-            if keyActionMap.get(key.char):
-                print('if char')
-                print(key)
-                listener._suppress = False
-                keyboard.tap(keyActionMap.get(key.char))
-                listener._suppress = True
-        # else:
-        #     print('else')
-        #     listener._suppress = False
-        #     keyboard.release(key.char)  
-        #     listener._suppress = True
-    
-        
-
-    except AttributeError as e:
-        print('err')
-        print(e)
-        if key == Key.esc:
-            exit()
-
-def on_release(key):
-    global caps
-    print("on release: ", key)
-    if key == Key.caps_lock:
-        # print("is caps release"  )
-        caps = False
-    if key == Key.esc:
-        exit()
+# Create the window
+window = sg.Window("Hooked", layout,size=(290, 300))
 
 
 
-# Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release,
-        suppress=True) as listener:
-    listener.joinw()
+def onPressE():
+    keyboard.send('enter')
+    print('ctrl E')
+
+def onCapsQ():
+    keyboard.send('backspace')
+    print('ctrl Q')
+
+def onCapsW():
+    keyboard.send('up')
+    print('ctrl W')
+
+def onCapsA():
+    keyboard.send('left')
+    print('ctrl A')
+
+def onCapsS():
+    keyboard.send('down')
+    print('ctrl S')
+
+def onCapsD():
+    keyboard.send('right')
+    print('ctrl D')
+
+def onCapsC():
+    keyboard.send('windows + ctrl + left')
+    print('ctrl C')
+
+def onCapsV():
+    keyboard.send('windows + ctrl + right')
+    print('ctrl V')
+
+def suppressInput():
+    # Stub to capture caps lock without fully blocking it
+    # We can listen to shift caps this way
+    print('Caps')
+
+def onShiftCaps():
+    keyboard.send('caps lock')
+    print('Caps')
+
+hasClickedRecently = False
+
+def setHasClickedTimerFalse():
+    global hasClickedRecently
+    hasClickedRecently = False
+
+
+
+t = Timer(1.0, setHasClickedTimerFalse)
+def onCtrlR():
+    global hasClickedRecently
+    if (hasClickedRecently == False):
+        hasClickedRecently = True
+        mouse.double_click()
+    else:
+        hasClickedRecently = False
+        mouse.click()
+
+capsLockCombos = ['', 'w + d', 'w + e', 's + d', 's + w', 'a + d' ]
+
+for otherKeys in capsLockCombos:
+    fullKeyCombo = 'caps lock'
+    if otherKeys != '':
+        fullKeyCombo = fullKeyCombo + ' + ' + otherKeys
+    keyboard.add_hotkey(fullKeyCombo, suppressInput, suppress=True)
+
+# list of combinations of keys that should be supressed
+# keyboard.add_hotkey('caps lock', suppressInput, suppress=True)
+# keyboard.add_hotkey('caps lock + w + d', suppressInput, suppress=True)
+# keyboard.add_hotkey('caps lock + w + e', suppressInput, suppress=True)
+# keyboard.add_hotkey('caps lock + s + d', suppressInput, suppress=True)
+# keyboard.add_hotkey('caps lock + s + w', suppressInput, suppress=True)
+# keyboard.add_hotkey('caps lock + a + d', suppressInput, suppress=True)
+
+
+keyboard.add_hotkey('caps lock + shift', onShiftCaps, suppress=True)
+keyboard.add_hotkey('caps lock + e', onPressE, suppress=True)
+keyboard.add_hotkey('caps lock + q', onCapsQ, suppress=True)
+keyboard.add_hotkey('caps lock + w', onCapsW, suppress=True)
+keyboard.add_hotkey('caps lock + a', onCapsA, suppress=True)
+keyboard.add_hotkey('caps lock + s', onCapsS, suppress=True)
+keyboard.add_hotkey('caps lock + d', onCapsD, suppress=True)
+keyboard.add_hotkey('caps lock + c', onCapsC, suppress=True)
+keyboard.add_hotkey('caps lock + v', onCapsV, suppress=True)
+
+keyboard.add_hotkey('ctrl + r', onCtrlR, suppress=True)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "Close" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+
+
+
+
+
+
+
 
